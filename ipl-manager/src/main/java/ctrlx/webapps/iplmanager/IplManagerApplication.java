8536b50b0408b7ab1d24ae609b9b5e32ac759def
msvithushan <msvithushan@gmail.com>
2021-05-15 17:13:13 +0530

    adding the spring project to the repo

---
+++ ipl-manager/src/main/java/ctrlx/webapps/iplmanager/IplManagerApplication.java
@@ -0,0 +1,13 @@
+package ctrlx.webapps.iplmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplManagerApplication.class, args);
+	}
+
+}
